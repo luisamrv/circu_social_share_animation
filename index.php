84b514d16c3c2f221874ffe398701144468dfa35
Unicorn <luisa.rodriguesv@gmail.com>
2020-05-04 15:59:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- html snippet -->
+<ul class="menu--animation-share-sidebar">
+	<li class="share top">
+	  <i class="fa fa-share-alt"></i>
+	    <ul class="submenu a2a_kit">
+	    <li><a class="a2a_button_pinterest"><i class="fab fa-pinterest-p"></i></a></li>
+	    <li><a class="a2a_button_whatsapp"><i class="fab fa-whatsapp"></i></a></li>
+	    <li><a class="a2a_button_facebook_messenger"><i class="fab fa-facebook-messenger"></i></a></li>
+	    <li><a class="a2a_button_facebook"><i class="fab fa-facebook-f"></i></a></li>
+	    <li><a class="a2a_button_twitter"><i class="fab fa-twitter"></i></a></li>
+	    </ul>
+	</li>
+</ul>
+<!-- end html snippet -->
